0bcd2961354728e78a1a1dbb7c81780d9dddbadf
Sean <sean.raborg@gmail.com>
2018-04-23 03:51:54 -0700

    Modified Target Files
    Set both game & editor targets to load the new testing module

--- Source/Testing/Testing.build.cs
+++ Source/Testing/Testing.build.cs
@@ -4,7 +4,14 @@ public class Testing : ModuleRules
 {
     public Testing( TargetInfo Target )
     {
+		// Include Module Dependencies
         PublicDependencyModuleNames.AddRange( new string[] { "Core", "Engine" } );
         PrivateDependencyModuleNames.AddRange( new string[] { "RenderCore" } );
+		
+		// Include module header files
+       PublicIncludePaths.AddRange(new string[] {"Testing/Public"});
+       
+       // Include module source files
+       PrivateIncludePaths.AddRange(new string[] {"Testing/Private"});
     }
 }
